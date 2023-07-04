43ac4f82caddab559944ecdb10fc2fb753aac78c
kamch4tskiy <kamchatskiymma@gmail.com>
2023-07-04 15:00:37 +0300

    04.07.2023#1: Added base files and uploaded some code into them

---
+++ Source/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+    Routes,
+    Route,
+    BrowserRouter,
+  } from "react-router-dom"
+import {Header} from "./Components/Header";
+import {Footer} from "./Components/Footer";
+
+export const App = () => 
+{
+  return 
+  (
+    <>
+        <BrowserRouter>
+
+        </BrowserRouter>
+    </>
+  );
+}
