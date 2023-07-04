bb093982c0df0eb0414550ef0133db58f0830da1
kamch4tskiy <kamchatskiymma@gmail.com>
2023-07-04 15:28:42 +0300

    04.07.2023#2: Changed icon, started working on header and connected to github

---
+++ src/Components/Header/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const ContainerHeader = styled.div
+`
+    height: 100px;
+    width: 100%;
+    background-color: #000000;
+    
+    text-align: center;
+    font-size: 50px;
+
+    border: 3px;
+    border-width: 5px;
+    border-style: dashed;
+`;
