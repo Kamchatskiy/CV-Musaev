0e68aa32fbacaba29f968d65af8e9410b18cd849
kamch4tskiy <kamchatskiymma@gmail.com>
2023-07-11 11:34:19 +0300

    Changes to commit script.sh for normal deployment

--- commit.sh
+++ commit.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 ./eslint.sh
 
-#Build Branch
-npm run build
+echo -p "Would you like to commit and push to build? (Y/n) " flag
+${flag:=Y}
+if [ $flag=="Y" || $flag =="y"]
+then
+    #Build Branch
+    npm run build
+    git checkout build
+    #git add --all build/ #ToDo
+    echo -p "Enter a build branch commit description: " description
+    read description
+    git commit -m "$description"
+    git push origin build
+    clear
+    echo "Pushed to build branch!"
+fi
  
 
 # Main Branch
 git checkout main
 git add --all
-echo "Pushing to master branch"
-echo -n "Enter a commit description: " 
-read description
+echo "Commiting to master branch"
+echo -p "Enter a main branch commit description: " description
+
 git commit -m "$description"
 git push origin main
